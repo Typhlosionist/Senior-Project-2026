e7d23310718803582547293e03bbb7f8074edb14
Daniel <daniel.feliciano@ufl.edu>
2026-04-14 13:09:56 -0400

    upgrade reset bug fix
    new game off of win screen didnt reset upgrades

--- Assets/UI/WinMenu.cs
+++ Assets/UI/WinMenu.cs
@@ -9,6 +9,13 @@ public class WinMenu : MonoBehaviour
     {
         Debug.Log("Replay");
         GameStateManager.Instance.currentLevel = 0;
+        Shooting.spreadOne = false;
+        Shooting.spreadTwo = false;
+        Bullter.flame = false;
+        Bullter.gust = false;
+        Bullter.freeze = false;
+        Bullter.wildfire = false;
+        Bullter.ice = false;
         SceneManager.LoadScene("SampleScene");
     }
     
